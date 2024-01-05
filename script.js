bb951bb43aaa67abff958159094d121c761e94be
Jm-joshmoll <Jm.joshmoll@gmail.com>
2024-01-05 16:49:22 +0000

    Update about and skills

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ var typed = new Typed(".multiple-text", {
     backDelay: 1000,
     loop: true
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('.navbar a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
